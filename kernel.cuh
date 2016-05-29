e567b4c6fd9b2936a3d711fbd4f70b5aef121293
Sagar Dolas <yl24ized@stud.informatik.uni-erlangen.de>
2016-05-29 16:40:42 +0200

    Added kernel

--- kernel.cuh
+++ kernel.cuh
@@ -3,11 +3,17 @@
 #include "Type.h"
 
 
-//Calculation of the Leonard Jones Potential
+// Calculation of the Leonard Jones Potential
 __device__ void lenardJonesPotential(const real_d *relativeVector,real_d * forceVector ,const real_d sigma , const real_d epislon) ;
 
-// Calculation of the forces
-__global__ void  calcForces(real_d *force,const real_d *position,const real_l numParticles ,const real_d sigma, const real_d epislon,const real_d rcutoff){
+// Calculation of the minimum forces
+__device__ void minDist(real_d* relativeVector,const real_d *p,const real_d *n, const real_d *cellLength) ;
+
+// Calculating the norm of the distance
+__device__ real_d norm(const real_d *vector) ;
+
+// Calculation of the forces for brute force calculation
+__global__ void  calcForces(real_d *force,const real_d *position,const real_l numParticles ,const real_d sigma, const real_d epislon,const real_d rcutoff, const real_d * const_args){
 
 
     real_l idx = threadIdx.x + blockIdx.x * blockDim.x ;
@@ -26,15 +32,19 @@ __global__ void  calcForces(real_d *force,const real_d *position,const real_l nu
 	force[i] = 0.0 ; 
     }
 
+    // Intialising the vector for cell length for x , y ,z directions
+    real_d cellLength[3] = {0.0} ;
+
+    // for loop to initialise the vector with appropriate values
+
     for (real_l i = 0 ; i < numParticles ; ++i){
         if(i != idx){
             // Find out the index of particle
             real_l vidxn = i * 3 ;
 
             // Find out the realtive vector
-            relativeVector[0] = position[vidxp] - position[vidxn] ;
-            relativeVector[1] = position[vidxp+1] - position[vidxn+1] ;
-            relativeVector[2] = position[vidxp+2] - position[vidxn+2] ;
+            // Relative vector updation has to be done on the minDist device function and it has to be removed from here
+            minDist(relativeVector,&position[vidxp],&position[vidxn], &const_args[6]);
 
             // Find the magnitude of relative vector and if it is less than cuttoff radius , then potential is evaluated otherwise , explicitly zero force is given
             // Magnitude of relative vector
@@ -50,21 +60,73 @@ __global__ void  calcForces(real_d *force,const real_d *position,const real_l nu
                 force[vidxp+1] +=  0.0 ;
                 force[vidxp+2] +=  0.0 ;
             }
-    }
+        }
     }
 }
 
-// Position update
-__global__ void updatePosition(const real_d *force,real_d *position,const real_d* velocity, const real_d * mass,const real_l numparticles,const real_d timestep ) {
+// Finding out the minimum distance between two particle keeping in mind the periodic boundary condition
+
+__device__ void minDist(real_d* relativeVector,const real_d *p,const real_d *n, const real_d * cellLength) {
+
+    // Dummy distance
+    real_d distold = cellLength[0] * cellLength[0] * cellLength[0] ;
+
+    // Temporary relative vector array
+    real_d tempRelativeVector[3] = {0.0} ;
+
+    // Iterating over all 27 possibilities to find out the minimum distance
+     for (real_d x = n[0] - cellLength[0] ; x <= n[0] + cellLength[0] ; x = x + cellLength[0] )
+         for (real_d y = n[1] - cellLength[1] ; y <= n[1] + cellLength[1] ; y = y+ celllength[1])
+             for (real_d z = n[2] - cellLength[2] ; z <= n[2] + cellLength[2] ; z = z + cellLength[2]) {
+
+                 // Finding out the relative vector
+                 tempRelativeVector[0] = p[0] - x ;
+                 tempRelativeVector[1] = p[1] - y ;
+                 tempRelativeVector[2] = p[2] - z ;
+
+                 // Find out the modulus of the distance
+                 real_d dist = norm(tempRelativeVector) ;
+
+                 // Holding the minimum value and finding out the minimum of all of them
+                 if(dist < distold){
+                     relativeVector[0] = p[0] - x ;
+                     relativeVector[1] = p[1] - y ;
+                     relativeVector[2] = p[2] - z ;
+                     distold = dist ;
+                 }
+
+            }
+
+}
+
+
+// Position update and taking care of periodic boundary conditions
+__global__ void updatePosition(const real_d *force,real_d *position,const real_d* velocity, const real_d * mass,const real_l numparticles,const real_d timestep, const
+                                real_d * const_args) {
 
     real_l idx = threadIdx.x + blockIdx.x * blockDim.x ;
     if(idx < numparticles ){
-    real_l vidx = idx * 3 ;
 
-    //(timestep * velocity[vidx] )   
-    position[vidx]   += (timestep * velocity[vidx] ) + ( (force[vidx] * timestep * timestep) / ( 2.0 * mass[idx]) ) ;
-    position[vidx+1] += (timestep * velocity[vidx+1] ) + ( (force[vidx+1] * timestep * timestep) / ( 2.0 * mass[idx]) ) ;
-    position[vidx+2] += (timestep * velocity[vidx+2] ) + ( (force[vidx+2] * timestep * timestep) / ( 2.0 * mass[idx]) ) ;
+        real_l vidx = idx * 3 ;
+
+        position[vidx]   += (timestep * velocity[vidx] ) + ( (force[vidx] * timestep * timestep) / ( 2.0 * mass[idx]) ) ;
+        position[vidx+1] += (timestep * velocity[vidx+1] ) + ( (force[vidx+1] * timestep * timestep) / ( 2.0 * mass[idx]) ) ;
+        position[vidx+2] += (timestep * velocity[vidx+2] ) + ( (force[vidx+2] * timestep * timestep) / ( 2.0 * mass[idx]) ) ;
+
+        // CHecking if the particle has left the physical domain direction wise ,for each direction
+
+        // Checking for the x direction
+        if (position[vidx] <= const_args[0]) position[vidx] += const_args[6] ;
+        if (position[vidx] >= const_args[1]) position[vidx] -= const_args[6] ;
+
+        // Checking for the y direction
+        if (position[vidx+1] <= const_args[2]) position[vidx] += const_args[7] ;
+        if (position[vidx+1] >= const_args[3]) position[vidx] -= const_args[7] ;
+
+        // CHecking for z direction and updating the same
+        if (position[vidx+2] <= const_args[4]) position[vidx] += const_args[8] ;
+        if (position[vidx+2] >= const_args[5]) position[vidx] -= const_args[8] ;
+
     }
 }
 
